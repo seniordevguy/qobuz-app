f69da9aef4fae1c6b2e8e950612ca13c38abdaa9
Robert Gallione <robert@gallionetech.com>
2023-12-25 21:07:33 -0500

    updates

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import logging
 import os
 from qobuz_dl.core import QobuzDL
-import requests
 import schedule
 import time
 from dotenv import load_dotenv
+import qobuz.api as qobuz_api
+import qobuz as qobuz_cl
 
 logging.basicConfig(level=logging.INFO)
 load_dotenv()
 
 qobuz_email = os.environ["QOBUZ_EMAIL"]
 qobuz_pasword = os.environ["QOBUZ_PASSWORD"]
+qobuz_app_id = os.environ["QOBUZ_APP_ID"]
 music_directory = os.environ.get("MUSIC_DIRECTORY", "/downloads")
 config_directory = os.environ.get("CONFIG_DIRECTORY", "/config")
 quality = int(os.environ.get("QUALITY", 27))
-lastfm_api_key = os.environ.get("LASTFM_API_KEY", "b3d36aca59dd6dfa595c7034c8b0fa59")
-lastfm_username = os.environ["LASTFM_USERNAME"]
 run_every_x_hours = int(os.environ.get("RUN_EVERY_X_HOURS", 1))
+
+# Or register your APP_ID and APP_SECRET
+qobuz_api.register_app(qobuz_app_id)
+qobuz_user = qobuz_cl.User(qobuz_email, qobuz_pasword)
 
 qobuz = QobuzDL(
     directory=music_directory,
@@ -25,66 +29,77 @@ qobuz = QobuzDL(
     folder_format="{artist}/{artist} - {album}",
 )
 
-def fetch_all_top_albums(api_key, user):
-    base_url = "http://ws.audioscrobbler.com/2.0/"
-    albums = []
+def get_user_favorites(user: qobuz_cl.User, fav_type, raw=False):
+    '''
+    Returns all user favorites
 
-    params = {
-        "method": "user.gettopalbums",
-        "user": user,
-        "api_key": api_key,
-        "format": "json",
-        "limit": 100
-    }
-
+    Parameters
+    ----------
+    user: dict
+        returned by qobuz.User
+    fav_type: str
+        favorites type: 'tracks', 'albums', 'artists'
+    limi
+    '''
+    limit = 50
+    offset = 0
+    favorites = list()
     while True:
-        response = requests.get(base_url, params=params)
-        data = response.json()
-        
-        # Add the albums from this page to your list
-        albums.extend(data['topalbums']['album'])
-
-        # Check if there's a next page
-        if '@attr' in data['topalbums'] and int(data['topalbums']['@attr']['page']) < int(data['topalbums']['@attr']['totalPages']):
-            params['page'] = int(data['topalbums']['@attr']['page']) + 1
+        favs = user.favorites_get(fav_type=fav_type, limit=limit, offset=offset, raw=raw)
+        if raw:
+            if len(favs[fav_type]["items"]) == 0:
+                break
+            for _f in favs[fav_type]["items"]:
+                favorites.append(_f)
         else:
-            break
-
-    formatted_albums = [f"{album['artist']['name']} - {album['name']}" for album in albums]
-    return formatted_albums
+            if not favs:
+                break
+            favorites += favs
+        offset += limit
+    return favorites
 
 def filter_existing_albums(albums):
     new_albums = []
     for album in albums:
-        # Split the album title at the '-' character and trim whitespace
-        parts = album.split('-')
-        artist = parts[0].strip() if parts else ""
         # Check if the path for the artist does NOT exist
-        if not os.path.exists(os.path.join(music_directory, artist, album)):
+        if not os.path.exists(os.path.join(music_directory, album['artist'], f"{album['artist']} - {album['title']}")):
             new_albums.append(album)
     return new_albums
-
-def get_album_ids(qobuz: QobuzDL, albums):
-    album_ids = []
-
-    for album in albums:
-        result = qobuz.search_by_type(album, "album", limit=1, lucky=True)
-        if len(result) == 1:
-            album_id = result[0].split('/')[-1]
-            album_ids.append(album_id)
-    return album_ids
 
 def download_album_ids(qobuz: QobuzDL, album_ids):
     for album_id in album_ids:
         qobuz.download_from_id(album_id)
 
 def job():
-    qobuz.get_tokens()
-    qobuz.initialize_client(qobuz_email, qobuz_pasword, qobuz.app_id, qobuz.secrets)
-    albums = fetch_all_top_albums(lastfm_api_key, lastfm_username)
-    new_albums = filter_existing_albums(albums)
-    ids = get_album_ids(qobuz, new_albums)
-    download_album_ids(qobuz, ids)
+    try:
+        # Initialize the Qobuz client
+        qobuz.get_tokens()
+        qobuz.initialize_client(qobuz_email, qobuz_pasword, qobuz.app_id, qobuz.secrets)
+
+        # Retrieve favorite albums
+        favorite_albums = get_user_favorites(qobuz_user, fav_type="albums", raw=True)
+
+        print(f"Retrieved {len(favorite_albums)} albums...")
+
+        # Parse albums
+        parsed_albums = []
+        for album in favorite_albums:
+            album_id = album['id']
+            album_info = {'id': album_id, 'title': album['title'], 'artist': album['artist']['name']}
+            parsed_albums.append(album_info)
+
+        # Filter new albums
+        new_albums = filter_existing_albums(parsed_albums)
+        print(f"Filtered Existing: Processing {len(new_albums)} albums...")
+
+        # Download new albums
+        new_album_ids = [album['id'] for album in new_albums]
+        download_album_ids(qobuz, new_album_ids)
+
+    except Exception as e:
+        # Handle exceptions (e.g., network issues, data access problems)
+        print(f"An error occurred: {e}")
+
 
 schedule.every(run_every_x_hours).hours.do(job)
 schedule.run_all()
